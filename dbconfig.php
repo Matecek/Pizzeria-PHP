d85c48e0c11cbb38aeade241d34675027837b6b6
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2024-07-30 15:15:13 +0200

    poprawa do sesji

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,4 @@
-<?php
-$server="bdykc6n2mhgs6zc3br7v-mysql.services.clever-cloud.com"; // nazwa serwera //localhost
+<?php $server="bdykc6n2mhgs6zc3br7v-mysql.services.clever-cloud.com"; // nazwa serwera //localhost
 $user="u8l5fhlzvhovmf6e"; // nazwa użytkownika //pizzeria
 $pass="M0PvfksgKIXrX7hFxRZX"; // hasło do użytkownika //NT6SKWtUPR1fQae1
-$base="bdykc6n2mhgs6zc3br7v"; // nazwa bazy danych //mup2023
-?> 
+$base="bdykc6n2mhgs6zc3br7v"; // nazwa bazy danych //mup2023 ?> 
